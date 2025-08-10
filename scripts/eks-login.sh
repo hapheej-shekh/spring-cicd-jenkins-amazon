26e4151d87cf7a0e25c00705de9e3f4aa998fd7a
hapheej-shekh <shapheej@gmail.com>
2025-08-10 12:49:38 +0530

    pipeline

--- scripts/eks-login.sh
+++ scripts/eks-login.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set -e
 
 # Replace with your actual cluster name and region
 CLUSTER_NAME="iam-eks-cluster"
 AWS_REGION="ap-south-1"
+PROFILE_USER="eks-admin"
 
-echo "[INFO] Getting cluster credentials..."
-aws eks update-kubeconfig --region $AWS_REGION --name $CLUSTER_NAME
+aws eks --region $AWS_REGION update-kubeconfig --name $CLUSTER_NAME --profile $PROFILE_USER --no-cli-pager
